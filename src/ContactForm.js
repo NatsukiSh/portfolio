f2d3606d9eeeac5a3002ba03e08cc83c21e760f9
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2023-02-22 22:25:13 +0100

    Adjusted basic contact me form

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -17,27 +17,44 @@ const ContactForm = () => {
   return (
     <div className="Contact-container">
       <h2>Contact Me</h2>
-      <h4>Got a question or proposal, or just want to say hello? Go ahead.</h4>
       <form onSubmit={onSubmit}>
-        <div>
+        <div className="FillingFormName">
           <label className="form-label" htmlFor="name">
             Name:
           </label>
-          <input className="form-control" type="text" id="name" required />
+          <input
+            className="form-control"
+            type="text"
+            id="name"
+            required
+            placeholder="Enter your name"
+          />
         </div>
-        <div>
+        <div className="FillingFormEmail">
           <label className="form-label" htmlFor="email">
-            Email Address:
+            Email:
           </label>
-          <input className="form-control" type="email" id="email" required />
+          <input
+            className="form-control"
+            type="email"
+            id="email"
+            required
+            placeholder="Enter your Email"
+          />
         </div>
-        <div>
+        <div className="FillingFormMessage">
           <label className="form-label" htmlFor="message">
             Message:
           </label>
-          <input className="form-control" type="text" id="message" required />
+          <input
+            className="form-control"
+            type="text"
+            id="message"
+            required
+            placeholder="Got a question or proposal, or just want to say hello? Go ahead."
+          />
         </div>
-        <button className="btn btn-outline-inform" type="submit">
+        <button className="btn btn-outline-info" type="submit">
           {formStatus}
         </button>
       </form>
