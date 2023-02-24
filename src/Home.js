2d52bb900eee96d0e79cd816e876f249ceb3253a
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2023-02-24 21:44:30 +0100

    Changed headline style

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="Home">
       <div className="container">
-        <h2>Welcome to my page, this is </h2>
+        <h3>Welcome to my page, this is </h3>
         <h1>Natsuki Shuko</h1>
         <p>Fronend Web Developer, based in Germany</p>
         <a href="#about" className="btn btn-outline-info">
